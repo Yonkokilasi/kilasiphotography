48d9d1cc6637dfb3868bca3eedf32b933af4e3b6
Yonkokilasi <yonkokilasi@gmail.com>
2017-07-07 07:36:42 +0300

    Galleria.io now runs on the silhoutte page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+  // options
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
 
+// element argument can be a selector string
+//   for an individual element
+var msnry = new Masonry( '.grid', {
+  // options
+});
+$('.grid').masonry({
+  // set itemSelector so .grid-sizer is not used in layout
+  itemSelector: '.grid-item',
+  // use element for option
+  columnWidth: '.grid-sizer',
+  percentPosition: true
+})
