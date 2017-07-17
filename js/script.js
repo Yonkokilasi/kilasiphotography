3f4641ef299640790879081d6b1d8af3bfe3605a
Yonkokilasi <yonkokilasi@gmail.com>
2017-07-17 16:44:40 +0300

    styled long exposure page

--- js/script.js
+++ js/script.js
@@ -1,31 +1,5 @@
-var elem = document.querySelector('.grid');
-var msnry = new Masonry( elem, {
-  // options
-  itemSelector: '.grid-item',
-  columnWidth: 200
-});
 
-// element argument can be a selector string
-//   for an individual element
-var msnry = new Masonry( '.grid', {
-  // options
-});
-$('.grid').masonry({
-  // set itemSelector so .grid-sizer is not used in layout
-  itemSelector: '.grid-item',
-  // use element for option
-  columnWidth: '.grid-sizer',
-  percentPosition: true
-});
 // init masonry
-var $grid = $('.grid').masonry({
-  // options..
-});
-// layout Masonry after each image loads
-$grid.imagesLoaded().progress( function() {
-  $grid.masonry('layout');
-});
-// init Masonry
 var $grid = $('.grid').masonry({
   itemSelector: '.grid-item',
   percentPosition: true,
@@ -33,5 +7,16 @@ var $grid = $('.grid').masonry({
 });
 // layout Masonry after each image loads
 $grid.imagesLoaded().progress( function() {
+  $grid.masonry('layout');
+});
+
+// layout Masonry after each image loads
+$grid.imagesLoaded().progress( function() {
+  $grid.masonry();
+});
+$grid.on( 'click', '.grid-item', function() {
+  // change size of item via class
+  $( this ).toggleClass('grid-item--gigante');
+  // trigger layout
   $grid.masonry();
 });
